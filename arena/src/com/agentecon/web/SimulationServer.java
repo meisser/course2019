f13013f8369abdd4eee718be122a62c008605aab
Luzius Meisser <luzius@meissereconomics.com>
2019-11-12 22:27:57 +0100

    adding second tag for no-funds config

--- arena/src/com/agentecon/web/SimulationServer.java
+++ arena/src/com/agentecon/web/SimulationServer.java
@@ -45,6 +45,7 @@ public class SimulationServer extends VisServer {
 				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "ex3-adaptive-1", true));
 				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "ex4-demographics-1", true));
 				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "ex4-demographics-2-price-interventions", true));
+				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "ex4-financial-economy-no-funds-1", true));
 				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "financial-economy-1", true));
 				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "financial-economy-no-funds-1", true));
 				this.simulations.add(new GitSimulationHandle("meisser", "course2019", "financial-economy-shock-1", true));
